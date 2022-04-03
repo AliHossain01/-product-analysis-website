d23954c9ec69aa81b25fd8b2ada795fd01861a93
AliHossain01 <alihossaincse01@gmail.com>
2022-04-04 02:02:33 +0600

    reviews items style with tailwind css

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -4,13 +4,28 @@ import './Review.css';
 const Review = (props) => {
     const { name, message, rate, image } = props.product;
     return (
-        <div className='product'>
-            <img src={image} alt="" />
-            <h4>{name}</h4>
-            <p>Review: {message}</p>
-            <p>Rating: {rate}</p>
+        <div className='shadow-lg rounded-2xl w-[250px] bg-white p-4'>
+
+            <div className='gap-4 flex justify-between items-center'>
+                <div className='flex-shrink-0'>
+                    <div>
+                        <img
+                            alt='profile'
+                            src={image}
+                            className='mx-auto object-cover rounded-full h-16 w-16 '
+                        />
+                    </div>
+                </div>
+                <div className=' flex flex-col justify-end'>
+                    <span className='text-gray-600 font-medium'>{name}</span>
+                    <span className='text-gray-400 text-xs'>{rate}</span>
+                    <span className='text-gray-400 text-xs'>{message}</span>
+                </div>
+            </div>
+
         </div>
     );
 };
 
 export default Review;
+
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -5,17 +5,22 @@ import Review from './../Review/Review';
 const Reviews = () => {
     const [products, setProducts] = useProducts();
     return (
-        <div className='flex gap-4 flex justify-between'>
-
+        <div className='px-4 pt-20 pb-24 mx-auto max-w-7xl md:px-2 bg-gray-200'>
             <p className='text-center text-3xl font-bold text-gray-800'>
-                What our Customer Says!!..
+                What our Customer Says!!.
+            </p>
+            <p className='text-center mb-12 text-xl font-normal text-gray-500 '>
+                Total Reviews: {products.length}
             </p>
 
-            {
-                products.map(product => <Review key={product.id} product={product}></Review>)
-            }
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 justify-items-center'>
+                {
+                    products.map(product => <Review key={product.id} product={product}></Review>)
+                }
+            </div>
         </div>
     );
 };
 
 export default Reviews;
+
