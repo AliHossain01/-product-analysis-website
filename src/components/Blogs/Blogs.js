f40467b96a8c8334e8af5e29269db5cfa332376b
AliHossain01 <alihossaincse01@gmail.com>
2022-04-04 16:17:00 +0600

    blog section done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -13,14 +13,16 @@ const Blogs = () => {
                 </h1>
                 <p className=' font-mono mt-2 mb-3 font-semibold text-gray-900'>
 
-                    Answer: Context API is</p>
+                    Answer: The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent. React.createContext() is used for create the Context. It returns a consumer and a provider. Provider is a component that provides the state to its children. It will hold the state and be the parent of all the components that might need that state. Consumer as it so happens is a component that consumes and uses the state. </p>
 
                 <h1 className=' font-mono mt-10 text-xl text-center md:text-2xl '>
                     2. What is Semantic Tag?
                 </h1>
                 <p className=' font-mono mt-2 mb-3 font-semibold text-gray-900'>
 
-                    Answer: Semantic Tag is</p>
+                    Answer:A semantic element clearly describes its meaning to both the browser and the developer. Some non-semantic elements like div,span tells nothing about it's content. But the semantic element like nav,header,article,footer clearly defines the meaning of it's content. By using semantic elements, we are helping the browser to understand the meaning of the content instead of just displaying it. By providing this extra level of clarity, HTML5 semantic elements also help search engines to read the page and find the required information faster.
+
+                </p>
 
 
 
